8ab099f1e7e61efac665bc236053ffa8b480c99e
Tri Wibowo <bowo.rezpector5@gmail.com>
2020-11-02 12:55:13 +0700

    membuat aplikasi wa

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Yuli Prihati",
+      message: "Apa kabar mu di sana",
+      time: "15.30",
+      avatarUrl:
+          "https://png.pngtree.com/png-clipart/20190614/original/pngtree-female-avatar-vector-icon-png-image_3725439.jpg"),
+  new ChatModel(
+      name: "Fiyona Melina J Sirait",
+      message: "kapan ketemu kita???",
+      time: "16.30",
+      avatarUrl:
+          "https://png.pngtree.com/png-clipart/20191121/original/pngtree-character-woman-portrait-avatar-watercolor-splatter-illustration-png-image_5148064.jpg"),
+  new ChatModel(
+      name: "Intan Liana Sari",
+      message: "Kita end",
+      time: "19.30",
+      avatarUrl:
+          "https://png.pngtree.com/png-clipart/20191121/original/pngtree-watercolor-hand-drawn-woman-portrait-avatar-watercolor-splatter-png-image_5148062.jpg"),
+  new ChatModel(
+      name: "Santi",
+      message: "Pamer Bojo",
+      time: "15.30",
+      avatarUrl:
+          "https://png.pngtree.com/png-clipart/20191121/original/pngtree-watercolor-hand-drawn-character-woman-portrait-avatar-watercolor-png-image_5148103.jpg"),
+  new ChatModel(
+      name: "Mayang Sari",
+      message: "Kapan kah kita bertemu lagi???",
+      time: "15.30",
+      avatarUrl:
+          "https://png.pngtree.com/png-clipart/20191121/original/pngtree-watercolor-hand-drawn-character-woman-portrait-avatar-watercolor-png-image_5148071.jpg"),
+  new ChatModel(
+      name: "Irna Sari",
+      message: "Apa kabar mu di sana",
+      time: "15.30",
+      avatarUrl:
+          "https://png.pngtree.com/png-clipart/20191121/original/pngtree-stories-vector-girl-streamer-live-video-streaming-online-streaming-video-social-png-image_5142228.jpg"),
+];
